8ffdbafa8fbe28ab92e7df315322cad462105978
Alexander Dadyka <sanekokokok@gmail.com>
2023-12-11 19:38:36 +0300

    Refactor: test_controller.go move to app/http/controllers/web

--- internal/routes/web.go
+++ internal/routes/web.go
@@ -1,10 +1,10 @@
 package routes
 
 import (
-	"github.com/d-alejandro/go-code-examples/pkg/app/http/controllers"
+	"github.com/d-alejandro/go-code-examples/internal/app/http/controllers/web"
 	"github.com/gin-gonic/gin"
 )
 
 func InitWebRoutes(router *gin.Engine) {
-	router.GET("/test", controllers.NewTestController().Test)
+	router.GET("/test", web.NewTestController().Test)
 }
