070c9e31c695f36c1a9946175e5f340e70a571bf
Alexander Dadyka <sanekokokok@gmail.com>
2024-01-01 21:58:22 +0300

    Add: create_orders_table migration

---
+++ internal/database/migrations/20231229201215_create_orders_table.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"github.com/d-alejandro/go-code-examples/internal/app/models"
+	"github.com/d-alejandro/go-code-examples/internal/database"
+	"github.com/pressly/goose/v3"
+	"strconv"
+)
+
+const autoIncrementIdStart = 10000001
+
+func init() {
+	goose.AddMigrationContext(upCreateOrdersTable, downCreateOrdersTable)
+}
+
+func upCreateOrdersTable(ctx context.Context, tx *sql.Tx) error {
+	db := database.Get()
+
+	err := db.Migrator().CreateTable(&models.Order{})
+	if err != nil {
+		return err
+	}
+
+	query := `ALTER SEQUENCE orders_id_seq RESTART WITH ` + strconv.Itoa(autoIncrementIdStart)
+	result := db.Exec(query)
+
+	return result.Error
+}
+
+func downCreateOrdersTable(ctx context.Context, tx *sql.Tx) error {
+	return database.Get().Migrator().DropTable(&models.Order{})
+}
