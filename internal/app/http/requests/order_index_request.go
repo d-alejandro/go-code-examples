f5b82ab05135bc4330a2b430b891dcaac9784477
Alexander Dadyka <sanekokokok@gmail.com>
2024-01-19 15:31:45 +0300

    Fix: validation rules to requests/order_index_request

--- internal/app/http/requests/order_index_request.go
+++ internal/app/http/requests/order_index_request.go
@@ -3,6 +3,6 @@ package requests
 type OrderIndexRequest struct {
 	Start      int    `form:"start" binding:"required|eq=0,numeric,min=0"`
 	End        int    `form:"end" binding:"required,numeric,min=1"`
-	SortColumn string `form:"sort_column" binding:"required"`
-	SortType   string `form:"sort_type" binding:"required"`
+	SortColumn string `form:"sort_column" binding:"required,oneof= id status rental_date guest_count created_at"`
+	SortType   string `form:"sort_type" binding:"required,oneof=asc desc"`
 }
