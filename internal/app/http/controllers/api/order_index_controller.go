c68f849ccbd7e029e32ad97e92b32bdf4199b89e
Alexander Dadyka <sanekokokok@gmail.com>
2024-01-19 22:37:19 +0300

    Fix: Index action to order_index_controller

--- internal/app/http/controllers/api/order_index_controller.go
+++ internal/app/http/controllers/api/order_index_controller.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"github.com/d-alejandro/go-code-examples/internal/app/http/requests"
+	"github.com/d-alejandro/go-code-examples/internal/app/presenters"
 	"github.com/d-alejandro/go-code-examples/internal/app/use_cases/interfaces"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"net/http"
 )
 
@@ -17,5 +20,16 @@ func NewOrderIndexController(useCase interfaces.OrderIndexUseCaseInterface) *Ord
 }
 
 func (controller *OrderIndexController) Index(context *gin.Context) {
-	context.String(http.StatusOK, controller.useCase.Execute())
+	orderIndexRequest := new(requests.OrderIndexRequest)
+
+	err := context.ShouldBindWith(orderIndexRequest, binding.Query)
+	if err != nil {
+		presenters.PresentErrorPresenter(context, http.StatusBadRequest, err)
+		return
+	}
+
+	context.JSON(
+		http.StatusOK,
+		gin.H{"message": controller.useCase.Execute()},
+	)
 }
