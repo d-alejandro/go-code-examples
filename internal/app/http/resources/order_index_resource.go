162f48cf6c448f42a26f73b78af93204656db566
Alexander Dadyka <sanekokokok@gmail.com>
2024-01-26 03:18:12 +0300

    Fix: order_index_resource

--- internal/app/http/resources/order_index_resource.go
+++ internal/app/http/resources/order_index_resource.go
@@ -1,13 +1,41 @@
 package resources
 
-import "github.com/d-alejandro/go-code-examples/internal/app/models"
+import (
+	"github.com/d-alejandro/go-code-examples/internal/app/models"
+	"time"
+)
 
 type OrderIndexResource struct {
-	ID uint `json:"id"`
+	ID             uint    `json:"id"`
+	AgencyName     string  `json:"agency_name"`
+	Status         string  `json:"status"`
+	IsConfirmed    bool    `json:"is_confirmed"`
+	IsChecked      bool    `json:"is_checked"`
+	RentalDate     *string `json:"rental_date"`
+	UserName       *string `json:"user_name"`
+	TransportCount int     `json:"transport_count"`
+	GuestCount     int     `json:"guest_count"`
+	AdminNote      *string `json:"admin_note"`
 }
 
 func NewOrderIndexResource(order models.Order) OrderIndexResource {
+	var rentalDate *string
+
+	if order.RentalDate != nil {
+		date := order.RentalDate.Format(time.DateOnly)
+		rentalDate = &date
+	}
+
 	return OrderIndexResource{
-		ID: order.ID,
+		ID:             order.ID,
+		AgencyName:     order.Agency.Name,
+		Status:         string(order.Status),
+		IsConfirmed:    order.IsConfirmed,
+		IsChecked:      order.IsChecked,
+		RentalDate:     rentalDate,
+		UserName:       order.UserName,
+		TransportCount: order.TransportCount,
+		GuestCount:     order.GuestCount,
+		AdminNote:      order.AdminNote,
 	}
 }
