923bf135ef6de5a921b10cb347167252cc61becb
Alexander Dadyka <sanekokokok@gmail.com>
2024-01-26 15:36:29 +0300

    Fix: agency and order models

--- internal/app/models/agency.go
+++ internal/app/models/agency.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Agency struct {
-	ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"type:varchar(255);not null;unique"`
-	Contact   *string
+	ID        uint            `gorm:"primaryKey"`
+	Name      string          `gorm:"type:varchar(255);not null;unique"`
+	Contact   *string         `gorm:"type:text"`
 	CreatedAt *time.Time      `gorm:"type:timestamp(0)"`
 	UpdatedAt *time.Time      `gorm:"type:timestamp(0)"`
 	DeletedAt *gorm.DeletedAt `gorm:"type:timestamp(0)"`
--- internal/app/models/order.go
+++ internal/app/models/order.go
@@ -8,21 +8,21 @@ import (
 
 type Order struct {
 	ID             uint              `gorm:"primaryKey"`
-	AgencyId       uint              `gorm:"not null;index:idx_orders_agency_id"`
+	AgencyId       uint              `gorm:"type:bigint;not null;index:idx_orders_agency_id"`
 	Agency         Agency            `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Status         types.OrderStatus `gorm:"type:varchar(255);not null;default:'waiting';check:status in ('canceled', 'completed', 'paid', 'prepayment', 'waiting')"`
-	IsChecked      bool              `gorm:"not null;default:false"`
-	IsConfirmed    bool              `gorm:"not null;default:false"`
+	IsChecked      bool              `gorm:"type:boolean;not null;default:false"`
+	IsConfirmed    bool              `gorm:"type:boolean;not null;default:false"`
 	RentalDate     *time.Time        `gorm:"type:timestamp(0)"`
 	GuestCount     int               `gorm:"type:integer;not null"`
 	TransportCount int               `gorm:"type:integer;not null"`
 	UserName       *string           `gorm:"type:varchar(255)"`
 	Email          string            `gorm:"type:varchar(255);not null"`
 	Phone          string            `gorm:"type:varchar(255);not null"`
-	Note           *string
-	AdminNote      *string
-	ConfirmedAt    *time.Time      `gorm:"type:timestamp(0)"`
-	CreatedAt      *time.Time      `gorm:"type:timestamp(0)"`
-	UpdatedAt      *time.Time      `gorm:"type:timestamp(0)"`
-	DeletedAt      *gorm.DeletedAt `gorm:"type:timestamp(0)"`
+	Note           *string           `gorm:"type:text"`
+	AdminNote      *string           `gorm:"type:text"`
+	ConfirmedAt    *time.Time        `gorm:"type:timestamp(0)"`
+	CreatedAt      *time.Time        `gorm:"type:timestamp(0)"`
+	UpdatedAt      *time.Time        `gorm:"type:timestamp(0)"`
+	DeletedAt      *gorm.DeletedAt   `gorm:"type:timestamp(0)"`
 }
