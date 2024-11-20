e705a6819eb1085bc74719cf261e68b51bf8e2e6
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-20 20:21:23 +0300

    Fix: presenters

--- internal/app/order/presenter/presenter.go
+++ internal/app/order/presenter/presenter.go
@@ -9,10 +9,7 @@ import (
 type OrderPresenter interface {
 	PresentOrder(*gin.Context, *models.Order)
 	PresentOrderList(*gin.Context, []*models.Order)
-	errorPresenter
-}
 
-type errorPresenter interface {
 	PresentError(ctx *gin.Context, statusCode int, errors any)
 }
 
