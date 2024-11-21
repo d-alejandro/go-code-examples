aafa6395a1dc9d035ce7274693b01c417118fe97
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-22 02:53:50 +0300

    Refactor: order model

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -1,17 +1,9 @@
 package config
 
-import "github.com/d-alejandro/go-code-examples/internal/pkg/models/types"
-
 const (
 	MessageInvalidID = "The ID parameter is invalid."
 
 	SQLXDriverName = "postgres"
-
-	Canceled   types.OrderStatus = "canceled"
-	Completed  types.OrderStatus = "completed"
-	Paid       types.OrderStatus = "paid"
-	Prepayment types.OrderStatus = "prepayment"
-	Waiting    types.OrderStatus = "waiting"
 
 	TableOrders               = "orders"
 	ColumnIDTableOrders       = "id"
