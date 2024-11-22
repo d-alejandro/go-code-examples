7f5d5cc9e274a7d9430f761669683d365f74a9d0
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-22 20:46:47 +0300

    Fix: models

--- internal/pkg/models/order.go
+++ internal/pkg/models/order.go
@@ -13,22 +13,22 @@ const (
 )
 
 type Order struct {
-	ID             uint        `gorm:"primaryKey"`
-	AgencyID       uint        `gorm:"type:bigint;not null;index:idx_orders_agency_id"`
-	Agency         Agency      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Status         OrderStatus `gorm:"type:varchar(255);not null;default:'waiting';check:status in ('canceled', 'completed', 'paid', 'prepayment', 'waiting')"` //nolint:revive
-	IsChecked      bool        `gorm:"type:boolean;not null;default:false"`
-	IsConfirmed    bool        `gorm:"type:boolean;not null;default:false"`
-	RentalDate     *time.Time  `gorm:"type:timestamp(0)"`
-	GuestCount     int         `gorm:"type:integer;not null"`
-	TransportCount int         `gorm:"type:integer;not null"`
-	UserName       *string     `gorm:"type:varchar(255)"`
-	Email          string      `gorm:"type:varchar(255);not null"`
-	Phone          string      `gorm:"type:varchar(255);not null"`
-	Note           *string     `gorm:"type:text"`
-	AdminNote      *string     `gorm:"type:text"`
-	ConfirmedAt    *time.Time  `gorm:"type:timestamp(0)"`
-	CreatedAt      *time.Time  `gorm:"type:timestamp(0)"`
-	UpdatedAt      *time.Time  `gorm:"type:timestamp(0)"`
-	DeletedAt      *time.Time  `gorm:"type:timestamp(0)"`
+	ID             int         `db:"id" gorm:"primaryKey"`
+	AgencyID       int         `db:"agency_id" gorm:"type:bigint;not null;index:idx_orders_agency_id"`
+	Agency         Agency      `db:"agency" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Status         OrderStatus `db:"status" gorm:"type:varchar(255);not null;default:'waiting';check:status in ('canceled', 'completed', 'paid', 'prepayment', 'waiting')"` //nolint:revive
+	IsChecked      bool        `db:"is_checked" gorm:"type:boolean;not null;default:false"`
+	IsConfirmed    bool        `db:"is_confirmed" gorm:"type:boolean;not null;default:false"`
+	RentalDate     time.Time   `db:"rental_date" gorm:"type:timestamp(0);not null"`
+	GuestCount     int         `db:"guest_count" gorm:"type:integer;not null"`
+	TransportCount int         `db:"transport_count" gorm:"type:integer;not null"`
+	UserName       *string     `db:"user_name" gorm:"type:varchar(255)"`
+	Email          string      `db:"email" gorm:"type:varchar(255);not null"`
+	Phone          string      `db:"phone" gorm:"type:varchar(255);not null"`
+	Note           *string     `db:"note" gorm:"type:text"`
+	AdminNote      *string     `db:"admin_note" gorm:"type:text"`
+	ConfirmedAt    *time.Time  `db:"confirmed_at" gorm:"type:timestamp(0)"`
+	CreatedAt      time.Time   `db:"created_at" gorm:"type:timestamp(0);not null"`
+	UpdatedAt      time.Time   `db:"updated_at" gorm:"type:timestamp(0);not null"`
+	DeletedAt      *time.Time  `db:"deleted_at" gorm:"type:timestamp(0)"`
 }
