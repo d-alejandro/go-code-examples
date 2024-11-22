334f1dd9c3c5c8834ef84a3343d6c0e832495c39
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-22 20:30:49 +0300

    Add: convert_date helper

---
+++ internal/pkg/helpers/convert_date.go
@@ -0,0 +1,31 @@
+package helpers
+
+import "time"
+
+func ConvertDate[Date time.Time | *time.Time, DateFormatted string | *string](
+	date Date,
+	layout string,
+) DateFormatted {
+	var dateFormatted DateFormatted
+
+	convertFunc := func(text string) DateFormatted {
+		return any(text).(DateFormatted)
+	}
+
+	if _, ok := any(dateFormatted).(string); !ok {
+		convertFunc = func(text string) DateFormatted {
+			return any(&text).(DateFormatted)
+		}
+	}
+
+	switch dateTime := any(date).(type) {
+	case time.Time:
+		return convertFunc(dateTime.Format(layout))
+	case *time.Time:
+		if dateTime != nil {
+			return convertFunc(dateTime.Format(layout))
+		}
+	}
+
+	return dateFormatted
+}
