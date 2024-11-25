f98049e86aabcc86fc4011cc6c4051bf01fd460c
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-25 21:25:35 +0300

    Fix: GetOrder action to repository

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -7,8 +7,4 @@ const (
 
 	DateTimeLayout = "02-01-2006 15:04:05"
 	DateLayout     = "02-01-2006"
-
-	TableOrders               = "orders"
-	ColumnIDTableOrders       = "id"
-	RelationAgencyTableOrders = "Agency"
 )
