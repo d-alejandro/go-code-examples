02451b33570322a75cef1e7fccee93ca5a8c47f5
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-25 03:33:28 +0300

    Add: ExecuteWithinTransaction help function

--- internal/pkg/helpers/transaction.go
+++ internal/pkg/helpers/transaction.go
@@ -1,26 +1,39 @@
 package helpers
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
 
-func CompleteTransaction(tx *sqlx.Tx, err error) {
-	if recoveryErr := recover(); recoveryErr != nil {
-		err = fmt.Errorf("recovered from panic: %v", recoveryErr)
-		rollback(tx, err)
-		return
-	} else if err != nil {
-		rollback(tx, err)
-		return
+func ExecuteWithinTransaction(ctx context.Context, db *sqlx.DB, function func(tx *sqlx.Tx) error) (err error) {
+	var tx *sqlx.Tx
+
+	if tx, err = db.BeginTxx(ctx, nil); err != nil {
+		return err
 	}
-	err = tx.Commit()
+
+	defer func() {
+		if recoveryErr := recover(); recoveryErr != nil {
+			err = fmt.Errorf("recovered from panic: %v", recoveryErr)
+			err = rollback(tx, err)
+		}
+	}()
+
+	err = function(tx)
+
+	if err != nil {
+		return rollback(tx, err)
+	}
+
+	return tx.Commit()
 }
 
-func rollback(tx *sqlx.Tx, err error) {
+func rollback(tx *sqlx.Tx, err error) error {
 	if rollbackErr := tx.Rollback(); rollbackErr != nil {
 		err = errors.Join(err, fmt.Errorf("rolling back transaction: %w", rollbackErr))
 	}
+	return err
 }
