9d0587af6e01f241f637594f37582211e0eb6420
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-26 04:37:52 +0300

    Fix: GetOrderList action

--- internal/app/order/repository/order_list.go
+++ internal/app/order/repository/order_list.go
@@ -1,31 +1,62 @@
 package repository
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/d-alejandro/go-code-examples/internal/config"
 	"github.com/d-alejandro/go-code-examples/internal/pkg/dto"
 	"github.com/d-alejandro/go-code-examples/internal/pkg/models"
-	"gorm.io/gorm/clause"
 )
 
-const sortTypeDesc = "desc"
-
-func (repository *orderRepository) GetOrderList(pagination *dto.PaginationDTO) []*models.Order {
+func (rep *orderRepository) GetOrderList(ctx context.Context, dto *dto.PaginationDTO) []*models.Order {
 	var orders []*models.Order
 
-	column := clause.Column{
-		Name: models.TableOrders + "." + pagination.GetSortColumn(),
+	if !rep.isValidSortColumn(dto.GetSortColumn()) {
+		return orders
 	}
 
-	orderByColumn := clause.OrderByColumn{
-		Column: column,
-		Desc:   pagination.GetSortType() == sortTypeDesc,
+	if !rep.isValidSortDirection(dto.GetSortType()) {
+		return orders
 	}
 
-	repository.db.
-		Preload(models.RelationAgencyTableOrders).
-		Order(orderByColumn).
-		Limit(pagination.GetLimitValue()).
-		Offset(pagination.GetOffsetValue()).
-		Find(&orders)
+	rawQuery := `
+select ag.id "agency.id", ag.name "agency.name", o.*
+  from orders o
+  join agencies ag on o.agency_id = ag.id 
+                  and ag.deleted_at is null
+ where o.deleted_at is null 
+ order by o.%s %s
+ limit $1 offset $2
+`
+	query := fmt.Sprintf(rawQuery, dto.GetSortColumn(), dto.GetSortType())
+
+	err := rep.db.SelectContext(ctx, &orders, query, dto.GetLimitValue(), dto.GetOffsetValue())
+
+	if err != nil {
+		return []*models.Order{}
+	}
 
 	return orders
+}
+
+func (rep *orderRepository) isValidSortColumn(name string) bool {
+	_, exists := map[string]struct{}{
+		config.SortColumnID:         {},
+		config.SortColumnStatus:     {},
+		config.SortColumnRentalDate: {},
+		config.SortColumnGuestCount: {},
+		config.SortColumnCreatedAt:  {},
+	}[name]
+
+	return exists
+}
+
+func (rep *orderRepository) isValidSortDirection(name string) bool {
+	_, exists := map[string]struct{}{
+		config.SortDirectionASC:  {},
+		config.SortDirectionDESC: {},
+	}[name]
+
+	return exists
 }
--- internal/config/constants.go
+++ internal/config/constants.go
@@ -7,4 +7,13 @@ const (
 
 	DateTimeLayout = "02-01-2006 15:04:05"
 	DateLayout     = "02-01-2006"
+
+	SortColumnID         = "id"
+	SortColumnStatus     = "status"
+	SortColumnRentalDate = "rental_date"
+	SortColumnGuestCount = "guest_count"
+	SortColumnCreatedAt  = "created_at"
+
+	SortDirectionASC  = "asc"
+	SortDirectionDESC = "desc"
 )
