a71fb557ffe4bfb33f16ae765b7beb13f8af09c6
Alexander Dadyka <sanekokokok@mail.ru>
2024-11-30 23:20:57 +0300

    Refactor: store_test

--- internal/app/order/handler/store_test.go
+++ internal/app/order/handler/store_test.go
@@ -19,74 +19,76 @@ import (
 )
 
 func TestPositiveStore(t *testing.T) {
-	ctx := &gin.Context{}
+	t.Run("Positive case", func(t *testing.T) {
+		ctx := &gin.Context{}
 
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+		controller := gomock.NewController(t)
+		defer controller.Finish()
 
-	fake := faker.New()
+		fake := faker.New()
 
-	minDate := time.Now()
-	maxDate := minDate.AddDate(config.DateYearsMin, config.DateMonthsMin, config.DateDaysMin)
-	userName := fake.Person().Name()
+		minDate := time.Now()
+		maxDate := minDate.AddDate(config.DateYearsMin, config.DateMonthsMin, config.DateDaysMin)
+		userName := fake.Person().Name()
 
-	orderStoreRequest := request.OrderStoreRequest{
-		AgencyName:     fake.Company().Name(),
-		RentalDate:     fake.Time().TimeBetween(minDate, maxDate),
-		GuestCount:     config.GuestCountMin,
-		TransportCount: config.TransportCountMin,
-		UserName:       &userName,
-		Email:          fake.Internet().Email(),
-		Phone:          fake.Phone().Number(),
-	}
+		orderStoreRequest := request.OrderStoreRequest{
+			AgencyName:     fake.Company().Name(),
+			RentalDate:     fake.Time().TimeBetween(minDate, maxDate),
+			GuestCount:     config.GuestCountMin,
+			TransportCount: config.TransportCountMin,
+			UserName:       &userName,
+			Email:          fake.Internet().Email(),
+			Phone:          fake.Phone().Number(),
+		}
 
-	mockRenderingHelper := mocks.NewMockRenderingHelper(controller)
-	mockRenderingHelper.
-		EXPECT().
-		RenderJSON(gomock.Any(), gomock.Any(), gomock.Any()).
-		DoAndReturn(func(_ *gin.Context, code int, obj any) {
-			assert.Equal(t, http.StatusOK, code)
+		mockRenderingHelper := mocks.NewMockRenderingHelper(controller)
+		mockRenderingHelper.
+			EXPECT().
+			RenderJSON(gomock.Any(), gomock.Any(), gomock.Any()).
+			DoAndReturn(func(_ *gin.Context, code int, obj any) {
+				assert.Equal(t, http.StatusOK, code)
 
-			responseBody, isOk := obj.(gin.H)
-			require.True(t, isOk)
-			require.NotNil(t, responseBody)
+				responseBody, isOk := obj.(gin.H)
+				require.True(t, isOk)
+				require.NotNil(t, responseBody)
 
-			resp, isExist := responseBody["data"]
-			require.True(t, isExist)
-			require.NotNil(t, resp)
+				resp, isExist := responseBody["data"]
+				require.True(t, isExist)
+				require.NotNil(t, resp)
 
-			orderResource, ok := resp.(*resource.OrderResource)
-			require.True(t, ok)
-			require.NotNil(t, orderResource)
+				orderResource, ok := resp.(*resource.OrderResource)
+				require.True(t, ok)
+				require.NotNil(t, orderResource)
 
-			assert.Equal(t, orderStoreRequest.AgencyName, orderResource.AgencyName)
-			assert.Equal(
-				t,
-				helpers.ConvertDate[time.Time, string](orderStoreRequest.RentalDate, config.DateLayout),
-				orderResource.RentalDate,
-			)
-			assert.Equal(t, orderStoreRequest.GuestCount, orderResource.GuestCount)
-			assert.Equal(t, orderStoreRequest.TransportCount, orderResource.TransportCount)
-			assert.Equal(t, orderStoreRequest.UserName, orderResource.UserName)
-			assert.Equal(t, orderStoreRequest.Email, orderResource.Email)
-			assert.Equal(t, orderStoreRequest.Phone, orderResource.Phone)
-		})
+				assert.Equal(t, orderStoreRequest.AgencyName, orderResource.AgencyName)
+				assert.Equal(
+					t,
+					helpers.ConvertDate[time.Time, string](orderStoreRequest.RentalDate, config.DateLayout),
+					orderResource.RentalDate,
+				)
+				assert.Equal(t, orderStoreRequest.GuestCount, orderResource.GuestCount)
+				assert.Equal(t, orderStoreRequest.TransportCount, orderResource.TransportCount)
+				assert.Equal(t, orderStoreRequest.UserName, orderResource.UserName)
+				assert.Equal(t, orderStoreRequest.Email, orderResource.Email)
+				assert.Equal(t, orderStoreRequest.Phone, orderResource.Phone)
+			})
 
-	present := presenter.NewOrderPresenter(mockRenderingHelper)
+		present := presenter.NewOrderPresenter(mockRenderingHelper)
 
-	mockValidatorHelper := mocks.NewMockValidationHelper(controller)
-	mockValidatorHelper.
-		EXPECT().
-		ValidateForm(gomock.Any(), gomock.Any()).
-		DoAndReturn(func(_ *gin.Context, req any) error {
-			reqPointer, isOk := req.(*request.OrderStoreRequest)
-			require.True(t, isOk)
-			require.NotNil(t, reqPointer)
+		mockValidatorHelper := mocks.NewMockValidationHelper(controller)
+		mockValidatorHelper.
+			EXPECT().
+			ValidateForm(gomock.Any(), gomock.Any()).
+			DoAndReturn(func(_ *gin.Context, req any) error {
+				reqPointer, isOk := req.(*request.OrderStoreRequest)
+				require.True(t, isOk)
+				require.NotNil(t, reqPointer)
 
-			*reqPointer = orderStoreRequest
-			return nil
-		})
+				*reqPointer = orderStoreRequest
+				return nil
+			})
 
-	handle := NewOrderHandler(uCase, present, mockValidatorHelper)
-	handle.Store(ctx)
+		handle := NewOrderHandler(uCase, present, mockValidatorHelper)
+		handle.Store(ctx)
+	})
 }
