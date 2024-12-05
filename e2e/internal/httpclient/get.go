8eb95a1e171bf26ba00c6711ec81036e91fcfd1e
Alexander Dadyka <sanekokokok@mail.ru>
2024-12-05 17:01:14 +0300

    Add: get method

---
+++ e2e/internal/httpclient/get.go
@@ -0,0 +1,18 @@
+package httpclient
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/d-alejandro/go-code-examples/e2e/internal/pkg/response"
+)
+
+func (cli *orderClient) Get(id int) (*response.OrderResponse, *http.Response, error) {
+	requestUrl := fmt.Sprintf("%s/api/orders/%d", cli.connection.HTTPServerUrl, id)
+
+	var orderResponse response.OrderResponse
+
+	httpResponse, err := cli.send(http.MethodGet, requestUrl, nil, &orderResponse)
+
+	return &orderResponse, httpResponse, err
+}
