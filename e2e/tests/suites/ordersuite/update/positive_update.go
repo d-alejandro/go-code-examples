165d0b440c344dfad0d4f81beff354f64903dfe8
Alexander Dadyka <sanekokokok@mail.ru>
2024-12-06 03:10:24 +0300

    Add: positive_update test

--- e2e/tests/suites/ordersuite/update/positive_update.go
+++ e2e/tests/suites/ordersuite/update/positive_update.go
@@ -1,8 +1,8 @@
 package update
 
 import (
-	"github.com/d-alejandro/go-code-examples/e2e/internal/httpclient"
 	"github.com/d-alejandro/go-code-examples/e2e/internal/pkg/request"
+	"github.com/d-alejandro/go-code-examples/e2e/internal/pkg/response"
 	"github.com/ozontech/allure-go/pkg/framework/provider"
 )
 
@@ -10,26 +10,44 @@ func (suite *OrderUpdateSuite) TestPositiveUpdate(t provider.T) {
 	t.Title("Update order with positive test result")
 	t.Parallel()
 
-	tt := httpclient.NewOrderClient()
+	var orderResponse *response.OrderResponse
 
-	orderUpdateRequest := &request.OrderUpdateRequest{
-		ID:             10000065,
-		GuestCount:     1,
-		TransportCount: 1,
-		UserName:       nil,
-		Email:          "test@test.ru",
-		Phone:          "71437854000",
-		Note:           nil,
-		AdminNote:      nil,
-	}
+	t.WithNewStep("Create order", func(stepCtx provider.StepCtx) {
+		orderResponse = suite.Steps.CreateOrder(stepCtx)
 
-	resp, zz, err := tt.Update(orderUpdateRequest)
+		stepCtx.Require().NotNil(orderResponse)
+		stepCtx.Require().NotEmpty(orderResponse.Data.ID)
+	})
 
-	if err != nil {
-	}
+	t.WithNewStep("Update order", func(stepCtx provider.StepCtx) {
+		updateRequest := &request.OrderUpdateRequest{
+			ID:             orderResponse.Data.ID,
+			GuestCount:     orderResponse.Data.GuestCount + 2,
+			TransportCount: orderResponse.Data.TransportCount + 1,
+			UserName:       nil,
+			Email:          orderResponse.Data.Email,
+			Phone:          orderResponse.Data.Phone,
+			Note:           nil,
+			AdminNote:      nil,
+		}
 
-	_ = zz
-	_ = err
-	_ = resp
+		updateResponse := suite.Steps.UpdateOrder(stepCtx, updateRequest)
 
+		stepCtx.Require().NotNil(updateResponse)
+	})
+
+	t.WithNewStep("Show Updated order", func(stepCtx provider.StepCtx) {
+		resp := suite.Steps.GetOrder(stepCtx, orderResponse.Data.ID)
+
+		stepCtx.Require().NotNil(resp)
+
+		stepCtx.Assert().EqualValues(resp.Data.ID, orderResponse.Data.ID)
+		stepCtx.Assert().EqualValues(resp.Data.TransportCount, orderResponse.Data.TransportCount+1)
+		stepCtx.Assert().EqualValues(resp.Data.GuestCount, orderResponse.Data.GuestCount+2)
+		stepCtx.Assert().Empty(resp.Data.UserName)
+		stepCtx.Assert().EqualValues(resp.Data.Email, orderResponse.Data.Email)
+		stepCtx.Assert().EqualValues(resp.Data.Phone, orderResponse.Data.Phone)
+		stepCtx.Assert().Empty(resp.Data.Note)
+		stepCtx.Assert().Empty(resp.Data.AdminNote)
+	})
 }
