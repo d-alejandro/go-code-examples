3b81e398e6367f810d5f96ace9e7fb4764cb902c
Alexander Dadyka <sanekokokok@mail.ru>
2024-12-06 16:03:13 +0300

    Refactor: fix linter bugs to package response

--- e2e/internal/pkg/response/order_list.go
+++ e2e/internal/pkg/response/order_list.go
@@ -1,16 +1,18 @@
 package response
 
 type OrderListResponse struct {
-	Data []struct {
-		ID             int     `json:"id"`
-		AgencyName     string  `json:"agency_name"`
-		Status         string  `json:"status"`
-		IsConfirmed    bool    `json:"is_confirmed"`
-		IsChecked      bool    `json:"is_checked"`
-		RentalDate     string  `json:"rental_date"`
-		UserName       *string `json:"user_name"`
-		TransportCount int     `json:"transport_count"`
-		GuestCount     int     `json:"guest_count"`
-		AdminNote      *string `json:"admin_note"`
-	} `json:"data"`
+	Data []Order `json:"data"`
+}
+
+type Order struct {
+	ID             int     `json:"id"`
+	AgencyName     string  `json:"agency_name"`
+	Status         string  `json:"status"`
+	IsConfirmed    bool    `json:"is_confirmed"`
+	IsChecked      bool    `json:"is_checked"`
+	RentalDate     string  `json:"rental_date"`
+	UserName       *string `json:"user_name"`
+	TransportCount int     `json:"transport_count"`
+	GuestCount     int     `json:"guest_count"`
+	AdminNote      *string `json:"admin_note"`
 }
